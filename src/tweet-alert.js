642419468514715f025a2498a9b9e23783f5be14
raiseandfall <matthew@jvst.us>
2014-05-14 18:17:52 -0700

    First working version

--- src/tweet-alert.js
+++ src/tweet-alert.js
@@ -4,16 +4,70 @@
 
 'use strict';
 
+var Stream = require('user-stream'),
+    events = require('events'),
+    util   = require('util'),
+    stream;
+
 function TweetAlert(opts) {
 
   if (!(this instanceof TweetAlert)) {
     return new TweetAlert(opts);
   }
 
+  events.EventEmitter.call(this);
+
+  this.opts = opts;
+
+  // New Twitter Stream
+  stream = new Stream(opts);
+
 }
 
-TweetAlert.prototype.log = function(opts) {
-	console.log('TweetAlert :: log : ', opts);
+//inherit
+util.inherits(TweetAlert, events.EventEmitter);
+
+TweetAlert.prototype.track = function() {
+
+  var tweetAlert = this;
+
+  // If missing parameters
+  if (typeof this.opts !== 'object') {
+    tweetAlert.emit('error', {
+      message: 'Missing parameters'
+    });
+    return false;
+  }
+
+  // If user specific
+  var screenNameFilter = this.opts.screen_name || false;
+  
+  // Start the stream
+  stream.stream();
+
+  // Called when stream connected
+  stream.on('connected', function() {
+    console.log('stream connected');
+  });
+
+  // Error handler
+  stream.on('error', function(data) {
+  	console.log('stream error : ', data);
+  });
+
+  // Called when data is received
+  stream.on('data', function(json) {
+  	// Filter by user
+    if (json.user) {
+      if (!screenNameFilter || (json.user.screen_name === screenNameFilter)) {
+        tweetAlert.emit('tweet', {
+          text: json.text,
+          user: json.user.screen_name,
+          id: json.user.id
+        });
+      }
+    }
+  });
 };
 
 module.exports = TweetAlert;
