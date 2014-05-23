b70d5640a1ab9a2fee9c29c611b70754a3afb575
raiseandfall <matthew@jvst.us>
2014-05-23 15:09:46 -0700

    Changed filename to watch for example.js

--- gulp/tasks/nodemon.js
+++ gulp/tasks/nodemon.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
     nodemon = require('gulp-nodemon');
 
 gulp.task('nodemon', function() {
-	nodemon({ script: 'index.js', ext: 'js', watch: ['./src/', 'index.js'] })
+	nodemon({ script: 'example.js', ext: 'js', watch: ['./src/', 'example.js'] })
     .on('change', ['lint'])
     .on('restart', function() {
       console.log('node restarted');
